261618005bce6532b382d3ab1adaad3502c76358
Adarsh Ravishankar <89323486+AdarshRavis@users.noreply.github.com>
2023-12-21 21:57:57 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from keras.models import load_model
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+# Load your trained model
+model = load_model('/Users/adarsh/Desktop/MacroImg/model_016950.h5')
+
+# Load and preprocess your custom image
+def load_and_preprocess_image(image_path, target_shape=(256, 256)):
+    # Load the image using OpenCV
+    custom_image = cv2.imread(image_path)
+    
+    # Resize the image to the desired shape
+    custom_image = cv2.resize(custom_image, target_shape)
+    
+    # Assuming the input image is in BGR format, swap the channels to RGB
+    custom_image = cv2.cvtColor(custom_image, cv2.COLOR_BGR2RGB)
+    
+    # Check if the image needs normalization (pixel values in [0, 255] range)
+    if custom_image.max() > 1.0:
+        # Normalize the pixel values to the range [-1, 1]
+        custom_image = (custom_image.astype(np.float32) - 127.5) / 127.50
+    
+    # Add a batch dimension
+    custom_image = np.expand_dims(custom_image, axis=0)
+    
+    return custom_image
+
+# Plot source and generated images
+def plot_images(src_img, gen_img):
+    images = np.vstack((src_img, gen_img))
+    # Scale from [-1,1] to [0,1]
+    images = (images + 1) / 2.0
+    titles = ['Original', 'Generated']
+    # Plot images row by row
+    for i in range(len(images)):
+        # Define subplot
+        plt.subplot(1, 2, 1 + i)
+        # Turn off axis
+        plt.axis('off')
+        # Plot raw pixel data
+        plt.imshow(images[i])
+        # Show title
+        plt.title(titles[i])
+    st.pyplot()
+
+# Streamlit app
+st.title("GAN Image Generator")
+
+# Upload custom image
+custom_image = st.file_uploader("Upload a custom image", type=["png", "jpg", "jpeg"])
+
+# Check if an image is uploaded
+if custom_image:
+    # Generate an image from your custom source
+    gen_image = model.predict(load_and_preprocess_image(custom_image))
+
+    # Plot both images together
+    plot_images(load_and_preprocess_image(custom_image), gen_image)
